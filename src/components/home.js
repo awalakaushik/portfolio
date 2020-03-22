f4d49e135c4a19fdc1d67dadec340f461412f5d2
awalakaushik <awalakaushikreddy@gmail.com>
2020-03-21 23:50:24 -0500

    add avataars package to add an avatar

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./home.css";
 
+import Avatar from 'avataaars';
+
 export default class Home extends React.Component {
     render() {
         return (
             <div className="home">
+                <Avatar
+                    avatarStyle='Transparent'
+                    topType='ShortHairShortWaved'
+                    accessoriesType='Round'
+                    hairColor='Black'
+                    facialHairType='BeardLight'
+                    facialHairColor='Black'
+                    clotheType='BlazerSweater'
+                    eyeType='Default'
+                    eyebrowType='Default'
+                    mouthType='Smile'
+                    skinColor='Yellow'
+                    />
                 Kaushik Reddy Awala
             </div>
         );
